a21e7ea310efa450deeb9b922df542a977185b77
Ibrahim Banat <ibrahim.banat.97@gmail.com>
2021-05-04 17:59:08 +0300

    finished the concepts

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+import faker from 'faker';
+const App = () => {
+  return (
+    <div className='ui container comments'>
+      <ApprovalCard>
+        <h4>Warning</h4>
+        Are you sure you want to do this?
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.internet.userName()}
+          timeAgo='Today at 4:45PM'
+          commentText={faker.lorem.sentence()}
+          imageSrc={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.internet.userName()}
+          timeAgo='Yesterday at 6:00PM'
+          commentText={faker.lorem.sentence()}
+          imageSrc={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.internet.userName()}
+          timeAgo='Today at 2:00AM'
+          commentText={faker.lorem.sentence()}
+          imageSrc={faker.image.avatar()}
+        />
+      </ApprovalCard>
+    </div>
+  );
+};
+ReactDOM.render(<App />, document.querySelector('#root'));
